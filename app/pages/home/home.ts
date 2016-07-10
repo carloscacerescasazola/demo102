e5bc9bf9222536ef914d6c8239f2944bffaa31d3
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-07-10 13:14:17 -0500

    start

---
+++ app/pages/home/home.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {Camera} from 'ionic-native';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+
+  image: string;
+
+  constructor() {}
+
+  getPicture(){
+    let options = {
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }
+    Camera.getPicture( options )
+    .then(imageData => {
+      this.image = `data:image/jpeg;base64,${imageData}`;
+    })
+    .catch(error =>{
+      console.error( error );
+    });
+  }
+}
